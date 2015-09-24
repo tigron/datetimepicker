c683f9c20d7f759b99b249032d9b736315b30580
Christophe Gosiau <christophe.gosiau@tigron.be>
2015-09-24 10:53:14 +0200

    add javascript file

---
+++ js/tigron-datetimepicker.js
@@ -0,0 +1,37 @@
+(function ($) {
+	$.fn.tigron-datetimepicker = function (options) {
+		if (typeof options.postFormat === 'undefined') {
+			options.postFormat = options.format;
+		}
+		var getInput = function(element) {
+		    if ($(element).is('input')) {
+		        input = $(element);
+		    } else {
+		        input = $(element).find($(element).data('DateTimePicker').datepickerInput());
+		        if (input.size() === 0) {
+		            input = $(element).find('input[type=text]');
+		        } else if (!input.is('input')) {
+		            throw new Error('CSS class "' + $(element).data('DateTimePicker').datepickerInput() + '" cannot be applied to non input element');
+		        }
+		    }
+		    return input;
+		}
+
+		cleaned_options = jQuery.extend(true, {}, options);
+		delete cleaned_options.postFormat;
+
+		$(this).datetimepicker(cleaned_options).on('dp.change', function(e) {
+			input = getInput( $(this) );
+			name = $(input).attr('data-old-name');
+			$('input[name="' + name + '"]').val(e.date.format(options.postFormat));
+		});
+
+		$.each($(this), function(element, value) {
+			input = getInput($(this));
+		    $(input).after( $('<input>').attr('type', 'hidden').attr('name', $(input).attr('name')).val($(this).data('DateTimePicker').date().format(options.postFormat)) );
+		    $(input).attr('data-old-name', $(input).attr('name'));
+			$(input).removeAttr('name');
+		})
+	};
+})(jQuery);
+
